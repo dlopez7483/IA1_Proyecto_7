9d751a2d527ff51040d1586638e9d90d52cf89da
david <davidlopezxd860@gmail.com>
2025-01-02 21:09:27 -0600

    changes

--- python/chat.py
+++ python/chat.py
@@ -20,7 +20,7 @@ translator = GoogleTranslator()
 model = None
 
 # Cargar datos
-intents_file = "intents2.json"  # Cambia esta ruta según corresponda
+intents_file = "intents3.json"  # Cambia esta ruta según corresponda
 model_file = "model.h5"       # Cambia esta ruta según corresponda
 
 with open(intents_file, 'r', encoding='utf-8') as content:
